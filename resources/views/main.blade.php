4afd3244277447edb4455f4c17e8c60614564f4e
NEFROSUR <107082043+NEFROSUR@users.noreply.github.com>
2022-06-15 19:50:28 -0500

    v5 3 update 15/06/2022

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -24,16 +24,16 @@
         <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
-        <div>    
-            <nav class="navbar navbar-expand-lg navbar-light bg-light" >
-                <img src="..." class="img-fluid" alt="...">
-                <a class="navbar-brand bg-warning p-2 text-dark bg-opacity-75 " href="/">NEFROSUR</a>
+        
+        
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark" >
+                <img src="http://assets.stickpng.com/images/585e4beacb11b227491c3399.png" bg-success p-2 text-white width="50" height="50" class="img-fluid" alt="...">
+                <a class="navbar-brand" href="/">NEFROSUR</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
-                <div class="collapse navbar-collapse bg-warning p-2 text-white bg-opacity-50"  id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0 " >
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="navigation" data-toggle="dropdown" aria-expanded="false" aria-label="Toggle navigation">
                             Pacientes
@@ -68,9 +68,9 @@
                             <a class="nav-link disabled">Administracion</a>
                         </li>
                     </ul>
-                </div>
+                
             </nav>
-        </div>     
+            
     </head>
     <body style="width:100%; height:100%;">
     
