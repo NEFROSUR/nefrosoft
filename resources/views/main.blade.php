9259a0ba52e4fded9a4c454f9380415b33082974
MejiaBryan <bmejia@unsa.edu.pe>
2022-08-08 17:42:58 -0500

    login 8/08/2022 v1

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,33 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
-    <title>Nefrosoft</title>
-    <style>
-        body {
-            margin: 16;
-            padding: 16;
-            font-family: sans-serif;
-        }
-
-        .color-container {
-            width: 20px;
-            height: 20px;
-            display: inline-block;
-            border-radius: 4px;
-        }
-    </style>
-    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
-
-
+@extends('welcome')
+    @section('main')
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <img src="http://assets.stickpng.com/images/585e4beacb11b227491c3399.png" bg-success p-2 text-white width="50" height="50" class="img-fluid" alt="...">
         <a class="navbar-brand" href="/">NEFROSUR</a>
@@ -92,7 +64,7 @@
     </div>
 </body>
 <footer>
-    <a>Desarrollado</a>
+<h1 class="text-5xl text-center pt-24">Bienvenido a la clinica Nefrologica del Sur NEFROSUR E.I.R.L.</h1>
 </footer>
 
-</html>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,33 +6,20 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\pacientesController;
 use App\Http\Controllers\PDFController;
 use App\Http\Controllers\ProfesionalController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionsController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('/mainNefrosur.principal');
 });
-//Route::get('generate-pdf', [PDFController::class, 'generatePDF']);
-/*Route::get('/main', function () {
-    return view('main');
+Route::get('/init', function () {
+    return view('/welcome');
 });
-
-Route::get('/',function(){
-    return view('principal');
-});*/
 Route::get('/recepcion/pdf', [App\Http\Controllers\FuaController::class, 'createPDF'])->name('fua.pdf');
 Route::resource('pacientes',pacientesController::class);
 Route::resource('profesionales',ProfesionalController::class);
-//Route::get('/pacientes.pacientesForTurno','pacientesController@pacientesPorTurno');
+
 
 Route::resource('recepcion',FuaController::class);
 Route::resource('historia',HistoriaController::class);
@@ -40,9 +27,21 @@ Route::resource('historia',HistoriaController::class);
 Route::get('/recepcion/pdfunit/{id}', [FuaController::class, 'createUnitPDF'])->name('fuaExtra.pdf');
 
 Route::get('/recepcion', [App\Http\Controllers\FuaController::class, 'index'])->name('recepcion.lisFua');
+Route::get('/register', [RegisterController::class, 'create'])
+    ->middleware('guest')
+    ->name('register.index');
 
+Route::post('/register', [RegisterController::class, 'store'])
+    ->name('register.store');
 
-/*Route::get('/recepcion/crearFua/pdf',function(){
-    return view('recepcion.crearFua');
-});*/
+Route::get('/login', [SessionsController::class, 'create'])
+    ->middleware('guest')
+    ->name('login.index');
+
+Route::post('/login', [SessionsController::class, 'store'])
+    ->name('login.store');
+
+Route::get('/logout', [SessionsController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('login.destroy');
 
