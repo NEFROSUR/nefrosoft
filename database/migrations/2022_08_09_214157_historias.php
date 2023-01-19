0375938440980bf2e9be9a5a7a5885787908b789
MejiaBryan <bmejia@unsa.edu.pe>
2023-01-19 09:15:20 -0500

    am

--- database/migrations/2022_08_09_214157_historias.php
+++ database/migrations/2022_08_09_214157_historias.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('historias', function (Blueprint $table) {
             $table->id('id')->autoincrement();
-            $table->unsignedBigInteger('correlativo');
+
             $table->date('fecha');
 
             
@@ -29,7 +29,6 @@ return new class extends Migration
             $table->string('ocurrencias')->nullable(); //heparinizacion
             $table->timestamps();
 
-            $table->foreign('correlativo')->references('correlativo')->on('fuas');
 
         });
     }
